cbacc0f8e30645564415748f4bc36091ed3eb16d
hyrkules <helouribeucherie@gmail.com>
2022-04-19 12:07:39 +0200

    fixing indentations

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -3,11 +3,11 @@
     <img src="../../images/logo.gif"/>
     <div>
       <!-- Création d'un nav bar -->
-        <nav class="content">
-          <RouterLink to="/" class="box">Episode</RouterLink>
-          <RouterLink to="/personnage" class="box">personnage</RouterLink>
-        </nav>
-        <RouterView />
-      </div>
+      <nav class="content">
+        <RouterLink to="/" class="box">Episode</RouterLink>
+        <RouterLink to="/personnage" class="box">personnage</RouterLink>
+      </nav>
+    <RouterView />
+    </div>
   </div>
 </template>
--- src/components/Personnage.vue
+++ src/components/Personnage.vue
@@ -33,16 +33,16 @@ export default defineComponent({
 <template>
   <div style="background-color:white">
     <!-- Pour chaque personnage, on affiche certaines de ses infos dans un grid -->
-      <div class="gridchar">
-        <div class="cardchar" v-for="character in infocharacters" v-bind:key="character.id"> 
-            <div class="" ><img :src=character.image></div>
-            <div class="" > 
-                <a v-bind:href="'/personnage/' + character.id"> {{ character.name }} </a>
-                <div>Status : {{character.status}}</div>
-                <div>Espèce : {{character.species}}</div>
-                <div>Lieu de vie : {{character.location.name}}</div>
-            </div>
+    <div class="gridchar">
+      <div class="cardchar" v-for="character in infocharacters" v-bind:key="character.id"> 
+        <div class="" ><img :src=character.image></div>
+        <div class="" > 
+          <a v-bind:href="'/personnage/' + character.id"> {{ character.name }} </a>
+          <div>Status : {{character.status}}</div>
+          <div>Espèce : {{character.species}}</div>
+          <div>Lieu de vie : {{character.location.name}}</div>
         </div>
+      </div>
     </div>
   </div>
 </template>
